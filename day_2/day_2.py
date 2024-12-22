29f7cb36d7ae4f7ec61dff715cd72d410366dbf2
Pawel <pawel.gurlo@gmail.com>
2024-12-22 09:17:09 +0100

    Second COmmit

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -9,9 +9,7 @@ with open("day_2/day_2_input.txt", "r") as f:
         l = [int(e) for e in l]
         data.append(l)
 
-num_of_safe = 0
-
-for line in data:
+def check_line_safe(line):
     increment = True
     increase = True
     # increase or decrease
@@ -20,7 +18,7 @@ for line in data:
     elif line[0] > line[1]:
         increase = False
     else:
-        continue
+        return False
 
     for i in range(len(line)-1):
         incremental_step = line[i+1] - line[i]
@@ -35,7 +33,75 @@ for line in data:
             increment = False
             break
 
+    return increment
+
+num_of_safe = 0
+
+for line in data:
+    if check_line_safe(line):
+        num_of_safe += 1
+
+print(num_of_safe)
+
+num_of_safe = 0
+
+problematic_levels = []
+problematic_levels_indexes = []
+
+# nr 2
+for line in data:
+    increment = True
+    increase = True
+    # increase or decrease
+    if line[0] < line[1]:
+        increase = True
+    elif line[0] > line[1]:
+        increase = False
+    else:
+        problematic.append(0)
+        problematic.append(1)
+        if line[0] < line[2]:
+            increase = True
+        elif line[0] > line[2]:
+            increase = False
+        else:
+            continue
+
+    problematic = []
+    for i in range(len(line)-1):
+        incremental_step = line[i+1] - line[i]
+
+        if not 1 <= abs(incremental_step) <= 3:
+            increment = False
+            if i not in problematic:
+                problematic.append(i)
+            problematic.append(i+1)
+            break
+        elif incremental_step <= 0 and increase:
+            increment = False
+            if i not in problematic:
+                problematic.append(i)
+            problematic.append(i+1)
+            break
+        elif incremental_step >= 0 and not increase:
+            increment = False
+            if i not in problematic:
+                problematic.append(i)
+            problematic.append(i+1)
+            break
+
     if increment:
         num_of_safe += 1
+    else:
+        problematic_levels.append(line)
+        problematic_levels_indexes.append(problematic)
+
+#print(problematic_levels, problematic_levels_indexes)
+for i in range(len(problematic_levels)):
+    for j in problematic_levels_indexes[i]:
+        if check_line_safe([problematic_levels[i][e] for e in range(len(problematic_levels[i])) if e != j]):
+            num_of_safe += 1
+            break
+
 
 print(num_of_safe)
